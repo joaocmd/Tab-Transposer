9e1b4b9d7b9929e4d6c06f64053441fe9c1f0e5a
João Carlos Morgado David <joaodavid1999@hotmail.com>
2018-06-05 12:02:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Tab Transposer
+
+This program is used to transpose text file guitar tabs.
+
+## Requirements
+
+You must have python installed in order to run the script (Written in python 3.6).
+
+## Instructions
+
+* Run the script, and it asks the name of the tab that you want to transpose. If your file is "tab.txt", type "tab", the files have to be under the same directory.
+* Next the program will ask for green light strings. This is useful if the tab has multiple playing guitars and you only want to change for example "Guitar 1". If no string is given, it will start transposing from the beggining of the file (it will replace all numbers, not just ones inside the tablature).
+* The next step is to give the programs the red lights. For example if there are three guitars and you only want to change the first one, give "Guitar 1" as a green light and "Guitar" as an invalid one, it will only check if it's invalid after checking if it's valid. This can also be used to stop transposing after a given line, such as "Verse 1".
+* Voila, it should be finished!
+* The new file will be caled "tab_new.txt" and will be on the same directory as the original "tab.txt" file.
+
+## Example:
+
+Simple example "tab".
+
+Given a file and these options:
+* Green lights: Guitar 1
+* Red lights: Guitar
+
+Original file:
+```
+Guitar 1
+|------6---7--8h9--5|
+|---5---------------|
+
+Guitar 2
+|------4---5--6h7--2|
+
+Guitar 1
+|------5---6--6h7---|
+|---5--------------5|
+```
+
+Creates a new file:
+```
+|Guitar 1
+|------1---2--3h4--0|
+|---0---------------|
+
+Guitar 2
+|------4---5--6h7--2|
+
+Guitar 1
+|------0---1--1h2---|
+|---0--------------0|
+```
---
+++ tab_transposer.py
@@ -0,0 +1,90 @@
+"""
+João Carlos Morgado David.
+
+This program takes a file and creates a new one, where the numbers of the tab are replaced
+by their transposed values. Asks the user which instruments he wants to be transposed, if
+none is given, it transposes over the whole file. If there are multiple instruments, the
+user has to give the program strings to identify when the instrument changes.
+
+Use at your own risk.
+"""
+import re
+
+def check_strs_in_line(line, strs):
+     """string x list of strings -> bool. Returns whether any of the strings in strs is part
+     of the string line."""
+
+     for str in strs:
+          if str in line:
+               return True
+     
+     return False
+
+def replace_numbers_in_line(line, transpose_value):
+     """string x int -> string. Returns a new string where the numbers found in line
+     are replaced by themselves - transpose_value."""
+     
+     # Sorting so that we start replacing the lower numbers so that there are
+     # no conflicts
+     numbers = sorted(set(int(i) for i in re.findall(r"\d+", line)))
+     for n in numbers:
+          new_number = n-transpose_value
+          new_number_str = str(n-transpose_value)
+          if n >= 10 and new_number < 10:
+               #Add a dash to compensate the lost character
+               new_number_str = "-" + new_number_str
+               
+          line = line.replace(str(n), new_number_str)
+          
+     return line
+
+def add_strings(l, msg):
+     """ list x string ->. Prints msg and appends each input line to l until it finds an emtpy
+     line."""
+     
+     print(msg)
+     while True:
+          s = input();
+          if s == "":
+               break
+               
+          l.append(s)    
+
+def main():
+     """->. This is where the main program runs."""
+     
+     file_name = input("File name (without \".txt\"): ") 
+     tranpose_value = eval(input("How many frets to transpose: "))
+     right_instrument = False
+     
+     valid_strs = []
+     print("Press just enter to finish (with no trailing whitespace).\n")
+     add_strings(valid_strs, "Green light strings (e.g: Guitar 1): ")
+     if (len(valid_strs) == 0):
+          #Start replacing from the beginning.
+          right_instrument = True
+     
+     invalid_strs = [] 
+     add_strings(invalid_strs, "Red light strings (e.g: Guitar): ")
+     
+     file = open(file_name + ".txt", "r")
+     new_file = open(file_name + "_new.txt", "w")
+     lines = file.readlines()
+     file.close()
+     
+     for line in lines:
+          if check_strs_in_line(line, valid_strs):
+               right_instrument = True
+               new_file.write(line)
+          elif check_strs_in_line(line, invalid_strs):
+               right_instrument = False
+               new_file.write(line)
+          elif right_instrument:
+               new_file.write(replace_numbers_in_line(line, tranpose_value))
+          else:
+               new_file.write(line)
+                  
+     new_file.close()
+     print("what")
+          
+main()
